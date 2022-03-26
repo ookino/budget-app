a5bb2480c9e9f38c51ad84b4f6e02f92fc72c60f
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-03-26 09:04:19 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/spalsh'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  root to: 'users#splash'
 end
