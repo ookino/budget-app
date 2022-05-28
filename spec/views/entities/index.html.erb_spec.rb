d2e72463bde093ec00cf5f196b1b6750f725ad92
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-05-28 08:01:06 +0100

    Generate specs

---
+++ spec/views/entities/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "entities/index", type: :view do
+  before(:each) do
+    assign(:entities, [
+      Entity.create!(),
+      Entity.create!()
+    ])
+  end
+
+  it "renders a list of entities" do
+    render
+  end
+end
