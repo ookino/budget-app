51798244193fecbf445eeb22a89f8f743530bab7
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-05-28 12:31:34 +0100

    Add specs for group and entity requests and model

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Yaseer Okino', email: 'y@mail.com', password: '12345678') }
+  let(:group) { user.groups.create(name: 'Party', icon: 'PT', color: 'red') }
+  let(:entity) do
+    user.entities.create(name: 'Party-transaction', amount: 100, date: '2022-05-28 09:45:22 UTC', group_id: group.id)
+  end
+
+  describe 'Validations' do
+    context 'when valid' do
+      it { expect(entity).to be_valid }
+    end
+
+    it 'should have a name' do
+      entity.name
+      expect(entity.name).to eq('Party-transaction')
+    end
+
+    it 'should have an amount' do
+      entity.amount
+      expect(entity.amount).to eq(100)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'Yaseer Okino', email: 'yaseer@mail.com', password: '12345678')
+  end
+  before { subject.save }
+
+  it 'name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the name' do
+    subject.name = 'Yaseer Okino'
+    expect(subject).to be_valid
+  end
 end
